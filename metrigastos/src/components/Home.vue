80abc0d0c0aeafcac66ba5b51452b53a506f1a8c
marisolalaro <marisolalaro23@gmail.com>
2023-10-08 22:47:23 -0400

    Header y contenido

--- metrigastos/src/components/Home.vue
+++ metrigastos/src/components/Home.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>holasss mundo</div>
+  <layout>
+    <template #header>
+      <Header></Header>
+    </template>
+  </layout>
 </template>
+
+<script>
+import layout from "./Layout.vue";
+import Header from "./Header.vue";
+export default {
+  components: {
+    layout,
+    Header,
+  },
+};
+</script>
