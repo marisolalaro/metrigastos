2682d1a93eeeb2323c92a5360f2f26073c6227c8
marisolalaro <marisolalaro23@gmail.com>
2023-10-11 05:04:09 -0400

    completacion funcionalidad

--- metrigastos/src/components/Home.vue
+++ metrigastos/src/components/Home.vue
@@ -3,16 +3,27 @@
     <template #header>
       <Header></Header>
     </template>
+    <template #resume>
+      <Resume />
+    </template>
+    <template #movements>
+      <Movements />
+    </template>
   </layout>
 </template>
 
 <script>
 import layout from "./Layout.vue";
 import Header from "./Header.vue";
+import Resume from "./Resume/Index.vue";
+import Movements from "./Movements.vue";
+
 export default {
   components: {
     layout,
     Header,
+    Resume,
+    Movements,
   },
 };
 </script>
